302677068db1b24366ca0f2dc711ac4fdb0fc1a7
BorisTax <tboris1983@gmail.com>
2024-02-21 15:20:53 +0200

    default image - no image

--- src/components/FasadSection.tsx
+++ src/components/FasadSection.tsx
@@ -1,7 +1,6 @@
-import React, { ReactElement } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import Fasad from "../classes/Fasad";
 import { Division } from "../types/enums";
-import { colors } from "../functions/materials";
 import FixedHeight from "./FixedHeight";
 import FixedWidth from "./FixedWidth";
 import FixedBoth from "./FixedBoth";
@@ -16,15 +15,19 @@ type FasadSectionProps = {
 }
 export default function FasadSection(props: FasadSectionProps): ReactElement {
     const fasad = props.fasad
+    const onlyFasad = fasad.Children.length === 0
     const rootFasad = props.rootFasad
     const activeFasad = props.activeFasad
+    const [backgroundBackupImage, setBackgroundBackupImage] = useState("")
     const setActiveFasad = useSetAtom(setActiveFasadAtom)
     const imageUrl = useImageUrl(fasad.ExtMaterial)
+    const fasadRef = useRef<HTMLDivElement>(null)
+    const nullRef = useRef<HTMLDivElement>(null)
     let gridTemplate: {
         gridTemplateColumns: string;
         gridTemplateRows: string;
     } = { gridTemplateRows: "1fr", gridTemplateColumns: "1fr" };
-    if (fasad.Children.length > 1) {
+    if (!onlyFasad) {
         const divHeight = fasad.Division === Division.HEIGHT
         const total = divHeight ? fasad.Height : fasad.Width
         const template = fasad.Children.map((f: Fasad) => `${(divHeight ? (f.Height + 1) / total : (f.Width + 1) / total).toFixed(3)}fr`).join(" ")
@@ -34,8 +37,7 @@ export default function FasadSection(props: FasadSectionProps): ReactElement {
     styles = { ...styles, border: (fasad === activeFasad ? "3px solid red" : "") }
     let events = {}
     let classes = ""
-    const backImage = `url("${imagesSrcUrl}${imageUrl}")`
-    if (fasad.Children.length === 0) {
+    if (onlyFasad) {
         styles = {
             ...styles,
             display: "flex",
@@ -43,9 +45,6 @@ export default function FasadSection(props: FasadSectionProps): ReactElement {
             flexShrink: "0",
             justifyContent: "center",
             alignItems: "center",
-            ariaLabel: "Нет изображения",
-            //backgroundColor: colors[fasad.Material],
-            backgroundImage: backImage,
             objectFit: "contain",
             cursor: "pointer",
         }
@@ -57,7 +56,20 @@ export default function FasadSection(props: FasadSectionProps): ReactElement {
     const fixedBoth = fixedHeight && fixedWidth
     const contents = fasad.Children.length > 1 ? fasad.Children.map((f: Fasad, i: number) => <FasadSection key={i} fasad={f} activeFasad={activeFasad} rootFasad={rootFasad} />) : ""
     const fixed = fixedBoth ? <FixedBoth /> : fixedHeight ? <FixedHeight /> : fixedWidth ? <FixedWidth /> : <></>
-    return <div className={classes} style={{
+    useEffect(() => {
+        if (fasadRef.current) setBackgroundBackupImage(fasadRef.current.style.backgroundImage)
+    }, [])
+    useEffect(() => {
+        const image = new Image()
+        const imageSrc = `${imagesSrcUrl}${imageUrl}`
+        const backImage = `url("${imageSrc}")`
+        image.src = imageSrc
+        if (fasadRef.current) fasadRef.current.style.backgroundImage = backgroundBackupImage
+        image.onload = () => {
+            if (fasadRef.current) fasadRef.current.style.backgroundImage = backImage
+        }
+    }, [imagesSrcUrl, imageUrl])
+    return <div ref={onlyFasad ? fasadRef : nullRef} className={classes} style={{
         display: "grid",
         ...gridTemplate,
         gap: "2px",
